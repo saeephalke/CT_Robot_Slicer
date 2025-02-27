1eeb119dfcb96179ff72690b598d8816ff9871c0
Saee Phalke <saeesphalke@gmail.com>
2025-02-27 10:07:55 -0800

    testing different values

--- slicer.py
+++ slicer.py
@@ -11,12 +11,12 @@ data = transform(data)
 data
 
 #creating a random vector 
-needle_vec = np.array([0, -0.5, 0.5])
+needle_vec = np.array([0, 0.5, 0.25]) #needle as a vector
 standard_i = np.array([1, 0, 0])
 needle_vec /= np.linalg.norm(needle_vec)
 norm_vec = np.cross(needle_vec, standard_i)
 print(norm_vec)
-point = np.array([0, 0, 0])
+point = np.array([0, 0, 0]) #position of the needle
 
 
 # plane coordinate in needle frame
